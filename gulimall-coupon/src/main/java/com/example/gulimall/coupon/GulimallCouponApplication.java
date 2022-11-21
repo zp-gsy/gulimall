17747e008df909cee17028201202ad23de37058d
zhaopeng <15689930853@163.com>
2022-11-21 22:11:31 +0800

    配置各个微服务数据库，测试基本CRUD功能

--- gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
@@ -1,14 +1,16 @@
 package com.example.gulimall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.gulimall.coupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
         //test
-        SpringApplication.run(gulimallCouponApplication.class, args);
+        SpringApplication.run(GulimallCouponApplication.class, args);
     }
 
 }
