c6f80205fddca1edd2d18ce9aa230c9789cd43f4
zhaopeng <15689930853@163.com>
2022-11-23 00:11:55 +0800

    Nacos注册中心，Nacos配置中心、feign远程调用、gateway网关

--- gulimall-order/src/main/java/com/example/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/example/gulimall/order/GulimallOrderApplication.java
@@ -3,9 +3,11 @@ package com.example.gulimall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.example.gulimall.order.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -3,9 +3,11 @@ package com.example.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.example.gulimall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
